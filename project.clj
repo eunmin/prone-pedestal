7a88784ec3c5eb4430cc3cf5e02ca5fc236b1919
eunmin <telepopsound@gmail.com>
2016-01-07 23:48:47 +0900

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prone-pedestal "0.1.0"
+(defproject prone-pedestal "0.1.1"
   :description "Prone for Pedestal"
   :url "http://github.com/eunmin/prone-pedestal"
   :license {:name "Eclipse Public License"
