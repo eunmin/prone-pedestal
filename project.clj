fdf36a85d5e7e7f98f5d2c809e981a022b04b6d3
eunmin <telepopsound@gmail.com>
2016-01-20 21:42:21 +0900

    Prepare project.clj for 0.1.3-SNAPSHOT development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prone-pedestal "0.1.2"
+(defproject prone-pedestal "0.1.3-SNAPSHOT"
   :description "Prone for Pedestal"
   :url "http://github.com/eunmin/prone-pedestal"
   :license {:name "Eclipse Public License"
